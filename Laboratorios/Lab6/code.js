7dd794ebd857a834c769761a70ee8f8825e650be
DanielQ <a01710441@itesm.mx>
2024-02-19 12:46:19 -0600

    Lab6

--- Laboratorios/Lab6/code.js
+++ Laboratorios/Lab6/code.js
@@ -1,24 +1,39 @@
+var input = document.getElementById("password");
+input.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      document.getElementById("boton").click();
+    }
+  });
+  
+
+
 function checkPassword(){
-    let password = document.getElementById("password"); 
-    let power = document.getElementById("power-point"); 
-    password.oninput = function () { 
-        let point = 0; 
-        let value = password.value; 
-        let widthPower =  
-            ["1%", "25%", "50%", "75%", "100%"]; 
-        let colorPower =  
-            ["#D73F40", "#DC6551", "#F2B84F", "#BDE952", "#3ba62f"]; 
-    
-        if (value.length >= 6) { 
+    let password = document.getElementById("password").value; 
+    let img = document.getElementById("image");
+    let points = 0;
+        if (password.length >= 6) { 
             let arrayTest =  
                 [/[0-9]/, /[a-z]/, /[A-Z]/, /[^0-9a-zA-Z]/]; 
             arrayTest.forEach((item) => { 
-                if (item.test(value)) { 
-                    point += 1; 
-                } 
+                if (item.test(password)) { 
+                    points +=1;
+                }
             }); 
+        }
+        
+        if(points >= 3){
+            img.src = "img/thumbsUp.png"
+            let texto = document.getElementById("quality")
+            texto.innerHTML = "Tu contraseña es segura";
+
+        }
+            else {
+                img.src ="img/thumbsDown.png" 
+                let texto = document.getElementById("quality")
+                texto.innerHTML = "Tu contraseña es insegura";
         } 
-        power.style.width = widthPower[point]; 
-        power.style.backgroundColor = colorPower[point]; 
-    }
 }
