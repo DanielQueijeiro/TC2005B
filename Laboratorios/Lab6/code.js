03e98bc36c7b6eb356a39104d78e66ebc4fbc94d
DanielQ <a01710441@itesm.mx>
2024-02-19 16:25:01 -0600

    Lab6

--- Laboratorios/Lab6/code.js
+++ Laboratorios/Lab6/code.js
@@ -1,10 +1,7 @@
 var input = document.getElementById("password");
 input.addEventListener("keypress", function(event) {
-    // If the user presses the "Enter" key on the keyboard
     if (event.key === "Enter") {
-      // Cancel the default action, if needed
       event.preventDefault();
-      // Trigger the button element with a click
       document.getElementById("boton").click();
     }
   });
