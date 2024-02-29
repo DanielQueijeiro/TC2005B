3731e99f5ef09559d67353ec30bf9da4bfc63072
DanielQ <a01710441@itesm.mx>
2024-02-29 17:06:52 -0600

    Fix

--- Laboratorios/Lab12/routes/pizzeria.routes.js
+++ Laboratorios/Lab12/routes/pizzeria.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/main', (request, response, next) => {
+router.get('/', (request, response, next) => {
   response.render('main'); 
 });
 
