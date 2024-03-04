f3458aa6aa38d530819c0797a56a53c3dadececf
DanielQ <a01710441@itesm.mx>
2024-03-04 09:58:47 -0600

    Arreglo y funcion

--- Laboratorios/Lab13/controllers/pizza.controllers.js
+++ Laboratorios/Lab13/controllers/pizza.controllers.js
@@ -1,3 +1,28 @@
+const menu = [{
+    nombre: "Pizza clasica", 
+    imagen: "https://images.ctfassets.net/n7hs0hadu6ro/1O0Be1dObiQBm17GQJHLj8/3fde720730f0b3616ecf5a82b928e7f9/pizza-a-domicilio-cerca-de-mi.jpg"
+}];
+
+function registrarPizza(string){
+    const filesystem = require('fs');
+    filesystem.writeFileSync('PizzaCreada.txt', string);
+}
+
+
 exports.get_pizzeria = (request, response, next) =>{
     response.render('pizzeria');
 };
+
+exports.post_pizza = (request, response, next) =>{
+    console.log(request.body);
+    menu.push(request.body);  
+    let ticket = "Nombre de la pizza: " + request.body.nombre + "\nImagen de la pizza: " + request.body.imagen; 
+    registrarPizza(ticket);
+    response.redirect('/menu');
+  };
+
+  exports.get_menu = (request, response, next) => {
+    response.render('menu', {
+        menu: menu
+    });
+  };
--- Laboratorios/Lab13/routes/crear.routes.js
+++ Laboratorios/Lab13/routes/crear.routes.js
@@ -1,33 +1,17 @@
-function registrarPizza(string){
-    const filesystem = require('fs');
-  
-    filesystem.writeFileSync('PizzaCreada.txt', string);
-  }
+
 
   const express = require('express');
 
   const router = express.Router();
   const pizzeriaController = require('../controllers/pizza.controllers')
   
-  const menu = [{
-    nombre: "Pizza clasica", 
-    imagen: "https://images.ctfassets.net/n7hs0hadu6ro/1O0Be1dObiQBm17GQJHLj8/3fde720730f0b3616ecf5a82b928e7f9/pizza-a-domicilio-cerca-de-mi.jpg"}];
+const menu = [{
+  nombre: "Pizza clasica", 
+  imagen: "https://images.ctfassets.net/n7hs0hadu6ro/1O0Be1dObiQBm17GQJHLj8/3fde720730f0b3616ecf5a82b928e7f9/pizza-a-domicilio-cerca-de-mi.jpg"}];
   
   
-  router.get('/menu', (request, response, next) => {
-    response.render('menu', {
-        menu: menu
-    });
-  });
-
-  router.post('/crearpizza', (request, response, next) =>{
-    console.log(request.body);
-    menu.push(request.body);  
-    let ticket = "Nombre de la pizza: " + request.body.nombre + "\nImagen de la pizza: " + request.body.imagen; 
-    registrarPizza(ticket);
-    response.redirect('/menu');
-  });
-  
+  router.get('/menu', pizzeriaController.get_menu);
+  router.post('/crearpizza',  pizzeriaController.post_pizza);
   router.get('/crearpizza', pizzeriaController.get_pizzeria);
 
   module.exports = router;
