0f0a1f8357837c23e244e54c2710702cf8517320
DanielQ <a01710441@itesm.mx>
2024-03-05 10:22:39 -0600

    Express session

--- Laboratorios/Lab14/controllers/pizza.controllers.js
+++ Laboratorios/Lab14/controllers/pizza.controllers.js
@@ -8,13 +8,20 @@ exports.post_pizza = (request, response, next) =>{
     console.log(request.body);
     const menu = new Menu(request.body.nombre, request.body.imagen);
     menu.save();
-    response.setHeader('Set-Cookie', 'ultima_pizza=' + request.body.nombre)
+    response.setHeader('Set-Cookie', 'ultima_pizza=' + request.body.nombre + '; HttpOnly')
     response.redirect('/menu');
   };
 
 exports.get_menu = (request, response, next) => {
+    let ultima_pizza = request.get('Cookie')
+    if(ultima_pizza){
+        ultima_pizza = ultima_pizza.split('=')[1];
+    } else { 
+        ultima_pizza = ''
+    }
     response.render('menu', {
-        menu: Menu.fetchAll()
+        menu: Menu.fetchAll(),
+        ultima_pizza: ultima_pizza,
     });
 };
 
