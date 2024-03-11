bf33797b0df03397b5545c7498fc754e73d470c2
DanielQueijeiro <dany.queijeiro@hotmail.com>
2024-03-11 11:12:12 -0600

    Lab 18 add

---
+++ Laboratorios/Lab18/models/usuario.model.js
@@ -0,0 +1,26 @@
+const db = require('../util/database');
+const bcrypt = require('bcryptjs');
+
+module.exports = class User {
+
+    constructor(mi_username, mi_password){
+        this.username = mi_username;
+        this.password = mi_password;
+    }
+
+    save() {
+        return bcrypt.hash(this.password, 12)
+        .then((password_cifrado) =>{
+            return db.execute('INSERT INTO usuario (username, password) VALUES (?, ?)',
+            [this.username, password_cifrado]);})
+        .catch((error => {
+            console.log(error)
+        }));
+
+    }
+
+    static fetchOne(username, password) {
+        return db.execute('SELECT * FROM usuario WHERE username=?', [username]);
+     }
+}
+
