8677fc67043505f86f24483a12d940036b0a8ca8
DanielQueijeiro <dany.queijeiro@hotmail.com>
2024-03-11 11:06:29 -0600

    Lab 18

--- Laboratorios/Lab18/controllers/usuarios.controller.js
+++ Laboratorios/Lab18/controllers/usuarios.controller.js
@@ -1,4 +1,5 @@
-const Usuario = require('../models/usuario.model')
+const Usuario = require('../models/usuario.model');
+const bcrypt = require('bcryptjs');
 
 exports.get_login = (request, response, next) =>{
     response.render('login', {
@@ -8,13 +9,27 @@ exports.get_login = (request, response, next) =>{
 }
 
 exports.post_login = (request, response, next) =>{
-    Usuario.fetchOne(request.body.username, request.body.password)
-    .then(([rows, fieldData]) =>{
-        if(rows.length == 1){
-            request.session.username = request.body.username;
-            response.redirect('/crear/menu');
+    Usuario.fetchOne(request.body.username)
+    .then(([users, fieldData]) => {
+        if(users.length == 1) {
+            //users[0] contiene el objeto de la respuesta de la consulta
+            const user = users[0];
+            bcrypt.compare(request.body.password, user.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        request.session.isLoggedIn = true;
+                        request.session.username = user.username;
+                        return request.session.save(err => {
+                            response.redirect('/crear/menu');
+                        });
+                    } else {
+                        return response.redirect('/users/login');
+                    }
+                }).catch(err => {
+                    response.redirect('/users/login');
+                });
         } else {
-            response.redirect('/usuarios/login')
+            response.redirect('/users/login')
         }
     })
     .catch(err => {
@@ -24,7 +39,7 @@ exports.post_login = (request, response, next) =>{
 
 exports.get_logout = (request, response, next) => {
     request.session.destroy(() => {
-        response.redirect('/'); //Este código se ejecuta cuando la sesión se elimina.
+        response.redirect('/users/login'); //Este código se ejecuta cuando la sesión se elimina.
     });
 };
 
