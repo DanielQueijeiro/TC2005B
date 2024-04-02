d904f2687702a235c5200e38245a133648dbb80c
DanielQueijeiro <dany.queijeiro@hotmail.com>
2024-04-02 10:41:15 -0600

    Lab24

---
+++ Laboratorios/Lab24/controllers/pizza.controllers.js
@@ -0,0 +1,54 @@
+const Menu = require('../models/pizza.model')
+
+exports.get_pizzeria = (request, response, next) =>{
+    response.render('pizzeria',{
+        username: request.session.username || '',
+        csrfToken: request.csrfToken(),
+        permisos: request.session.permisos || [],
+    });
+};
+
+exports.post_pizza = (request, response, next) =>{
+    const menu = new Menu(request.body.nombre, request.file.filename, request.body.username);
+    menu.save()
+        .then(([rows, fieldData]) => {
+            response.setHeader('Set-Cookie', 'ultima_pizza=' + request.body.nombre + '; HttpOnly')
+            response.redirect('/crear/menu');})
+        .catch((error) => {console.log(error)});
+  };
+
+exports.get_menu = (request, response, next) => {
+    let ultima_pizza = request.get('Cookie')
+    if(ultima_pizza){
+        ultima_pizza = ultima_pizza.split('=')[1];
+    } else { 
+        ultima_pizza = ''
+    }
+
+    Menu.fetch(request.params.pizza_id).then(([rows, fieldData]) => {
+        response.render('menu', {
+        menu: rows,
+        ultima_pizza: ultima_pizza,
+        username: request.session.username || '',
+        permisos: request.session.permisos || [],
+        })
+    })
+        .catch(error => {
+        console.log(error);
+    });
+};
+
+exports.get_main = (request, response, next) => {
+    response.render('main',{
+        username: request.session.username || '',
+        permisos: request.session.permisos || [],
+    }); 
+};
+
+exports.get_buscar = (request, response, next) => {
+    Menu.search(request.params.valor_busqueda)
+    .then(([pizzas, fieldData]) => {
+        return response.status(200).json({pizzas: pizzas});
+    })
+    .catch((error) => {console.log(error)})
+}
---
+++ Laboratorios/Lab24/models/pizza.model.js
@@ -0,0 +1,41 @@
+const filesystem = require('fs');
+
+const db = require('../util/database');
+
+module.exports = class Menu {
+
+    constructor(mi_nombre, mi_imagen, mi_username){
+        this.nombre = mi_nombre;
+        this.imagen = mi_imagen;
+        this.username = mi_username;
+    }
+
+    save() {
+        let ticket = "Nombre de la pizza: " + this.nombre + "\nImagen de la pizza: " + this.imagen; 
+        filesystem.writeFileSync('PizzaCreada.txt', ticket);
+        return db.execute('INSERT INTO pizza (nombre, imagen, username) VALUES (?, ?, ?)',
+        [this.nombre, this.imagen, this.username]
+    );
+    }
+    
+    static fetchAll() {
+       return db.execute('SELECT * FROM pizza');
+    }
+
+    static fetchOne(id) {
+        return db.execute('SELECT * FROM pizza WHERE id=?', [id]);
+     }
+    
+    static fetch(id){
+        if(id){
+            return this.fetchOne(id);
+        } else {
+            return this.fetchAll();
+        }
+    }
+
+    static search(valor_busqueda){
+        return db.execute(`SELECT * FROM pizza WHERE nombre LIKE ?`, ['%' + valor_busqueda + '%'])
+    }
+}
+
